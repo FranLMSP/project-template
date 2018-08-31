275aab22d0d5b0221f38a66baeaff2729193c5aa
Franco Colmenarez <francolmenagar2@hotmail.com>
2018-08-31 17:56:49 -0400

    Dropdown en el navbar header

--- resources/assets/js/components/views/AppHeader.vue
+++ resources/assets/js/components/views/AppHeader.vue
@@ -1,8 +1,18 @@
 <template>
-
+    <b-nav pills>
+      <b-nav-item active>Active</b-nav-item>
+      <b-nav-item>Link</b-nav-item>
+      <b-nav-item-dropdown id="nav7_ddown" text="Dropdown" extra-toggle-classes="nav-link-custom" right>
+        <b-dropdown-item>Mi perfil</b-dropdown-item>
+        <b-dropdown-divider></b-dropdown-divider>
+        <b-dropdown-item><fa :icon="icon.powerOff"></fa> Cerrar sesión</b-dropdown-item>
+      </b-nav-item-dropdown>
+    </b-nav>
 </template>
 
 <script type="text/javascript">
+
+    import { faPowerOff } from '@fortawesome/free-solid-svg-icons'
 
     export default {
         name: 'app-header',
@@ -19,7 +29,11 @@
             },
         },
         computed: {
-
+            icon() {
+                return {
+                    powerOff: faPowerOff
+                }
+            }
         },
         created() {
 
