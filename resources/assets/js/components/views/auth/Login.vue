4d818661f5868ef96641d63cc555740a315deeda
Franco Colmenarez <francolmenagar2@hotmail.com>
2018-08-31 14:02:13 -0400

    Limpiando vistas

--- resources/assets/js/components/views/auth/Login.vue
+++ resources/assets/js/components/views/auth/Login.vue
@@ -1,65 +1,12 @@
 <template>
-    <container fluid>
-        <br>
-        <row>
-            <column col="12" md="6" class="offset-md-3">
-                <card>
-                    <card-body>
-                        <form @submit.prevent="authenticate">
-                            <p class="h4 text-center py-4">Iniciar sesión</p>
-                            <div class="grey-text">
-                                <md-input v-model="form.username" label="Usuario" icon="user" group type="text" />
-                                <md-input v-model="form.password" label="Contraseña" icon="lock" group type="password" validate/>
-                            </div>
-
-                            <div class="text-center py-4 mt-3">
-                                <btn :disabled="loading" color="cyan" type="submit">Iniciar Sesión</btn>
-                            </div>
-
-                            <div v-if="error">
-                                <p style="color: red"><strong>Usuario o contraseña inválidos</strong></p>
-                            </div>
-                            <div v-if="success">
-                                ¡Bienvenido! Por favor espere
-                            </div>
-
-                        </form>
-                    </card-body>
-                </card>
-            </column>
-        </row>
-    </container>
 
 </template>
 
 <script type="text/javascript">
     import {login} from '../../../helpers/auth.js'
 
-    import {
-        Row,
-        Column,
-        Container,
-        Card,
-        CardImg,
-        CardBody,
-        CardTitle,
-        CardText,
-        MdInput,
-        Btn
-    } from 'mdbvue'
-
     export default {
         components: {
-            Row,
-            Column,
-            Container,
-            Card,
-            CardImg,
-            CardBody,
-            CardTitle,
-            CardText,
-            MdInput,
-            Btn
         },
         data() {
             return {
