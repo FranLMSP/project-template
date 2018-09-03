31ff59f151558debd1a264701f034e2ba4633809
Franco Colmenarez <francolmenagar2@hotmail.com>
2018-09-03 08:53:15 -0400

    Agregando métodos a la vista de listar

--- resources/assets/js/components/views/users/List.vue
+++ resources/assets/js/components/views/users/List.vue
@@ -1,10 +1,46 @@
 <template>
 
-            <b-card>
-            testaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-            </b-card>
+    <b-card>
+        testaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+    </b-card>
 </template>
 
 <script type="text/javascript">
-    export default {}
+
+export default {
+    name: 'UsersList',
+    data() {
+        return {
+            users: [],
+            loading: false,
+            error: false
+        }
+    },
+    methods: {
+        list() {
+            this.loading = true
+            this.error = false
+
+            axios.get('/api/users')
+                .then( res => {
+                    console.log(res)
+                })
+                .catch( err => {
+                    this.error = true
+                })
+                .then( () => {
+                    this.loading = false
+                })
+        },
+    },
+    computed: {
+        currentUser() {
+            return this.$store.getters.currentUser
+        },
+    },
+    created() {
+        this.list();
+    }
+}
+
 </script>
