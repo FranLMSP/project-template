5698afc87cdbc4d4df2fc27b98c2fa0373c5518f
Franco Colmenarez <francolmenagar2@hotmail.com>
2018-09-04 18:58:07 -0400

    Modulos ahora se muestran recursivamente

--- resources/assets/js/components/views/AppHeader.vue
+++ resources/assets/js/components/views/AppHeader.vue
@@ -33,73 +33,9 @@
 
         <div id="sidebar" class="sidebar-fixed position-fixed">
             <a class="logo-wrapper"><img alt="" class="img-fluid" src=""/></a>
-
-                <template v-for="module in modules" v-if="!module.api">
-                    <b-card no-body>
-                        <b-card-header header-tag="header" class="p-0" role="tab">
-                            <router-link
-                                v-if="module.childs.length == 0"
-                                class="btn btn-block btn-secondary"
-                                :to="module.url"
-                            >
-                                {{ module.name }}
-                            </router-link>
-                            <b-btn
-                                v-else
-                                block
-                                v-b-toggle="'c'+module.id"
-                            >
-                                {{ module.name }}
-                            </b-btn>
-                        </b-card-header>
-
-                        <b-collapse :id="'c'+module.id" v-if="module.childs.length > 0">
-                            <b-card-body>
-                                <template v-for="child in module.childs" v-if="!child.api">
-                                    <b-card no-body>
-                                        <b-card-header header-tag="header" class="p-0" role="tab">
-                                            <router-link
-                                                v-if="child.childs.length == 0"
-                                                class="btn btn-block btn-secondary"
-                                                :to="child.url"
-                                            >
-                                                {{ child.name }}
-                                            </router-link>
-                                            <b-btn
-                                                v-else
-                                                block
-                                                v-b-toggle="'c'+child.id"
-                                            >
-                                                {{ child.name }}
-                                            </b-btn>
-                                        </b-card-header>
-
-                                        <b-collapse :id="'c'+child.id" v-if="child.childs.length > 0">
-                                            <b-card-body>
-                                                <template v-for="grandchild in child.childs" v-if="!grandchild.api">
-                                                    <b-card no-body>
-                                                        <b-card-header header-tag="header" class="p-0" role="tab">
-                                                            <router-link
-                                                                class="btn btn-block btn-secondary"
-                                                                :to="grandchild.url"
-                                                            >
-                                                                {{ grandchild.name }}
-                                                            </router-link>
-                                                        </b-card-header>
-
-                                                    </b-card>
-                                                </template>
-                                            </b-card-body>
-                                        </b-collapse>
-
-                                    </b-card>
-                                </template>
-                            </b-card-body>
-                        </b-collapse>
-
-                    </b-card>
-                </template>
-
+            <template v-for="module in modules" v-if="!module.api">
+                <header-menu :module="module" ></header-menu>
+            </template>
         </div>
     </header>
 </template>
@@ -107,10 +43,12 @@
 <script type="text/javascript">
 
 import { faPowerOff } from '@fortawesome/free-solid-svg-icons'
+import HeaderMenu from './HeaderMenu.vue'
 
 export default {
     name: 'app-header',
     components: {
+        HeaderMenu
     },
     data() {
         return {
---
+++ resources/assets/js/components/views/HeaderMenu.vue
@@ -0,0 +1,45 @@
+<template>
+
+    <b-card no-body>
+        <b-card-header header-tag="header" class="p-0" role="tab">
+            <router-link
+                v-if="module.childs.length == 0"
+                class="btn btn-block btn-secondary"
+                :to="module.url"
+            >
+                {{ module.name }}
+            </router-link>
+            <b-btn
+                v-else
+                block
+                v-b-toggle="'c'+module.id"
+            >
+                {{ module.name }}
+            </b-btn>
+        </b-card-header>
+
+        <b-collapse :id="'c'+module.id" v-if="module.childs.length > 0">
+            <b-card-body>
+                <template v-for="child in module.childs" v-if="!child.api">
+                    <header-menu :module="child">
+                    </header-menu>
+                </template>
+            </b-card-body>
+        </b-collapse>
+
+    </b-card>
+
+
+</template>
+
+<script type="text/javascript">
+    
+export default {
+    props: [ 'active', 'module' ],
+    name: 'header-menu',
+    created() {
+        console.log(this.module)
+    }
+}
+
+</script>
